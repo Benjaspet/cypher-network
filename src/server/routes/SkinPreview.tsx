98ad66bffc658862434204d864ba2e780f2a46da
Benjaspet <benpetrillo.bp@gmail.com>
2024-07-25 18:40:32 -0400

    fix(dependency): stable @elysiajs/html version
    the @latest of this package is unstable

--- src/server/routes/SkinPreview.tsx
+++ src/server/routes/SkinPreview.tsx
@@ -1,41 +1,29 @@
+import Html from "@kitajs/html";
 import Elysia, { Context } from "elysia";
-
 import { logger } from "@app/CypherNetwork";
 
 /**
- * @api [GET] /api/v1/preview/:id
+ * @api [GET] /api/v1/preview?vid={vid}&name={name}
  * @description Gets a skin's preview video.
  */
-async function getPreview({
-    query: { vid, name }
-}: Context<{ query: { vid: string; name: string } }>) {
-    logger.info(
-        `Previewing ${decodeURIComponent(name)} video: ${decodeURIComponent(vid)}`
-    );
-    try {
-        return (
-            <html lang={"en"}>
-            <head>
-                <title>Preview</title>
-                <meta
-                    name={"viewport"}
-                    content={"width=device-width, initial-scale=1"}
-                />
-                <meta
-                    name={"og:title"}
-                    content={decodeURIComponent(name).replaceAll("\n", " - ")}
-                />
-                <meta name={"theme-color"} content={"#26867c"} />
-                <meta
-                    name={"twitter:player"}
-                    content={decodeURIComponent(vid)}
-                />
-                <meta name={"twitter:player:width"} content={"1280"} />
-                <meta name={"twitter:player:height"} content={"720"} />
-                <meta name={"twitter:card"} content={"player"} />
-                <meta name={"twitter:site"} content={"Cypher Network"} />
-                <style>
-                    {`
+async function getPreview({ query: { vid, name } }: Context<{ query: { vid: string, name: string } }>) {
+    logger.info(`Previewing ${decodeURIComponent(name)} video: ${decodeURIComponent(vid)}`);
+    const skinName: string = decodeURIComponent(name).replaceAll("\n", " - ");
+    const video: string = decodeURIComponent(vid);
+    return (
+        <html lang={"en"}>
+        <head>
+            <title>Preview</title>
+            <meta name={"viewport"} content={"width=device-width, initial-scale=1"} />
+            <meta name={"og:title"} content={skinName} />
+            <meta name={"theme-color"} content={"#26867c"} />
+            <meta name={"twitter:player"} content={video} />
+            <meta name={"twitter:player:width"} content={"1280"} />
+            <meta name={"twitter:player:height"} content={"720"} />
+            <meta name={"twitter:card"} content={"player"} />
+            <meta name={"twitter:site"} content={"Cypher Network"} />
+            <style>
+                {`
                 body {
                     margin: 0;
                     padding: 0;
@@ -50,34 +38,16 @@ async function getPreview({
                     height: 75%;
                 }
                 `}
-                </style>
-            </head>
-            <body>
-            <video controls>
-                <source src={decodeURIComponent(vid)} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-            </body>
-            </html>
-        );
-    } catch (e) {
-        logger.error(`An error occurred: ${e}`);
-        return (
-            <html lang={"en"}>
-            <head>
-                <title>Error</title>
-                <meta
-                    name={"viewport"}
-                    content={"width=device-width, initial-scale=1"}
-                />
-                <meta name={"theme-color"} content={"#26867c"} />
-            </head>
-            <body>
-            <h1>Oops! An error occurred.</h1>
-            </body>
-            </html>
-        );
-    }
+            </style>
+        </head>
+        <body>
+        <video controls>
+            <source src={video} type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
+        </body>
+        </html>
+    );
 }
 
 export default function (app: Elysia): Elysia {
